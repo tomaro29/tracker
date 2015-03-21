f6be39d981dbbc8228a11cf2f6b20dba62ebd8de
tony <maro2229@yahoo.fr>
2015-03-21 11:18:56 +0100

    [Add] checkstyle configuration
    Signed-off-by: tony <maro2229@yahoo.fr>

--- fr.rostren.tracker.model.editor/src/fr/rostren/tracker/presentation/dev/TrackerEditorDev.java
+++ fr.rostren.tracker.model.editor/src/fr/rostren/tracker/presentation/dev/TrackerEditorDev.java
@@ -1,34 +1,49 @@
 package fr.rostren.tracker.presentation.dev;
 
+import fr.rostren.tracker.Tracker;
+import fr.rostren.tracker.presentation.TrackerEditor;
+
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 
-import fr.rostren.tracker.Tracker;
-import fr.rostren.tracker.presentation.TrackerEditor;
-
+/**
+ * A custom tracker editor.
+ * 
+ * @author maro
+ *
+ */
 public class TrackerEditorDev extends TrackerEditor {
 
+	/**
+	 * Returns the edited tracker.
+	 * 
+	 * @return the tracker
+	 */
 	public Tracker getEditedTracker() {
+		Tracker tracker = null;
 		if (editingDomain != null) {
 			ResourceSet resourceSet = editingDomain.getResourceSet();
-			if (resourceSet == null)
+			if (resourceSet == null) {
 				throw new IllegalArgumentException(new NullPointerException(
 						"Invalid null ResourceSet"));
+			}
 
 			for (Resource resource : resourceSet.getResources()) {
-				if (resource == null)
+				if (resource == null) {
 					throw new IllegalArgumentException(
 							new NullPointerException("Invalid null Resource"));
+				}
 
-				if (resource.getContents().isEmpty())
+				if (resource.getContents().isEmpty()) {
 					return null;
+				}
 
 				final EObject root = resource.getContents().get(0);
-				return (root instanceof Tracker) ? (Tracker) root : null;
+				tracker = (root instanceof Tracker) ? (Tracker) root : null;
 			}
 		}
 
-		return null;
+		return tracker;
 	}
 }

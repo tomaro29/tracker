1432c7d220a608e5393f0216a122fd3a124f1864
tony <maro2229@yahoo.fr>
2016-05-11 23:11:26 +0200

    Use preferences store

--- fr.rostren.tracker.ui/src/fr/rostren/tracker/ui/handlers/ImportPDFHandler.java
+++ fr.rostren.tracker.ui/src/fr/rostren/tracker/ui/handlers/ImportPDFHandler.java
@@ -16,6 +16,8 @@ import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.emf.edit.domain.AdapterFactoryEditingDomain;
 import org.eclipse.emf.edit.ui.action.LoadResourceAction.LoadResourceDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.PreferenceStore;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.window.Window;
@@ -34,8 +36,9 @@ import fr.rostren.tracker.presentation.dev.TrackerEditorDev;
 import fr.rostren.tracker.ui.dialogs.CheckAndEditOperationWizard;
 
 public class ImportPDFHandler extends AbstractHandler implements IHandler {
+	private static final String PDF_URI_TEXT_FIELD_KEY = "PDF.URI.text.field.key.settings."; //$NON-NLS-1$
 	private Shell shell;
-	private String uriText;
+	private String pdfURIText;
 	private CheckingAccount account;
 	List<Operation> addedOperations = new ArrayList<Operation>();
 	Set<Origin> addedOrigins = new HashSet<Origin>();
@@ -43,28 +46,27 @@ public class ImportPDFHandler extends AbstractHandler implements IHandler {
 	@Override
 	public Object execute(ExecutionEvent event) {
 		Object applicationContext = event.getApplicationContext();
-		Object currentShell = HandlerUtil.getVariable(applicationContext,
-				ISources.ACTIVE_SHELL_NAME);
+		Object currentShell = HandlerUtil.getVariable(applicationContext, ISources.ACTIVE_SHELL_NAME);
 		if (!(currentShell instanceof Shell)) {
 			return null;
 		}
 		setShell((Shell) currentShell);
-		IStructuredSelection selection = (IStructuredSelection) HandlerUtil
-				.getCurrentSelection(event);
+		IStructuredSelection selection = (IStructuredSelection) HandlerUtil.getCurrentSelection(event);
 		if (selection instanceof StructuredSelection) {
+			IPreferenceStore preferenceStore = new PreferenceStore();
 			for (Iterator<?> objects = selection.iterator(); objects.hasNext();) {
-				Object selectedElement = AdapterFactoryEditingDomain
-						.unwrap(objects.next());
+				Object selectedElement = AdapterFactoryEditingDomain.unwrap(objects.next());
 				if (selectedElement instanceof CheckingAccount) {
 					this.account = (CheckingAccount) selectedElement;
 					// opens dialog to load a pdf
-					LoadResourceDialog dialog = new LoadResourceDialog(
-							getShell());
+					// TODO implement the LoadResourceDialog to set the PDF URI
+					// Text field content.
+					LoadResourceDialog dialog = new LoadResourceDialog(getShell());
 					int result = dialog.open();
 
 					if (result == Window.OK) {
-						this.uriText = dialog.getURIText();
-						if (uriText.isEmpty()) {
+						this.pdfURIText = dialog.getURIText();
+						if (pdfURIText.isEmpty()) {
 							MessageDialog.openError(shell, "Empty PDF URI.", //$NON-NLS-1$
 									"The pdf uri cannot be empty."); //$NON-NLS-1$
 
@@ -75,8 +77,7 @@ public class ImportPDFHandler extends AbstractHandler implements IHandler {
 				}
 			}
 		}
-		TrackerEditorDev editor = (TrackerEditorDev) HandlerUtil
-				.getActiveEditor(event);
+		TrackerEditorDev editor = (TrackerEditorDev) HandlerUtil.getActiveEditor(event);
 		editor.doSave(new NullProgressMonitor());
 		resetHandler();
 		return null;
@@ -84,7 +85,7 @@ public class ImportPDFHandler extends AbstractHandler implements IHandler {
 
 	private void resetHandler() {
 		account = null;
-		uriText = null;
+		pdfURIText = null;
 		addedOperations.clear();
 		addedOrigins.clear();
 	}
@@ -92,29 +93,22 @@ public class ImportPDFHandler extends AbstractHandler implements IHandler {
 	private void importOperationsToModel() {
 		// read the pdf file, extract data and edit operations
 		// categories.
-		PDFContentExtractor extractor = new PDFContentExtractor(uriText,
-				account);
+		PDFContentExtractor extractor = new PDFContentExtractor(pdfURIText, account);
 
 		boolean extracted = extractOperations(extractor);
 		if (addedOperations.isEmpty()) {
-			MessageDialog
-					.openError(shell,
-							"Cannot Import PDF", //$NON-NLS-1$
-							"The PDF is not valid, please make sure that the selection : '" //$NON-NLS-1$
-									+ uriText
-									+ "' has a correct format or contains at least one valid operation."); //$NON-NLS-1$
+			MessageDialog.openError(shell, "Cannot Import PDF", //$NON-NLS-1$
+					"The PDF is not valid, please make sure that the selection : '" //$NON-NLS-1$
+							+ pdfURIText + "' has a correct format or contains at least one valid operation."); //$NON-NLS-1$
 			return;
 		}
 
 		boolean editAborted = editOperations();
 		if (editAborted) {
-			TrackerUtils.getTracker(account).getOriginsRepository()
-					.getOrigins().removeAll(addedOrigins);
+			TrackerUtils.getTracker(account).getOriginsRepository().getOrigins().removeAll(addedOrigins);
 			account.getOperations().removeAll(addedOperations);
-			MessageDialog
-					.openInformation(shell,
-							"Abort PDF Import Action", //$NON-NLS-1$
-							"The Current PDF Import Action is aborted! All new origins and operations will be cleaned from the model."); //$NON-NLS-1$
+			MessageDialog.openInformation(shell, "Abort PDF Import Action", //$NON-NLS-1$
+					"The Current PDF Import Action is aborted! All new origins and operations will be cleaned from the model."); //$NON-NLS-1$
 			return;
 		}
 
@@ -141,12 +135,10 @@ public class ImportPDFHandler extends AbstractHandler implements IHandler {
 			}
 			done = true;
 		} catch (ExtractorException e) {
-			MessageDialog.openError(getShell(),
-					"Problem while extracting operations", //$NON-NLS-1$
+			MessageDialog.openError(getShell(), "Problem while extracting operations", //$NON-NLS-1$
 					e.getMessage());
 		} catch (IOException e) {
-			MessageDialog.openError(getShell(),
-					"Problem while opening the PDF File", //$NON-NLS-1$
+			MessageDialog.openError(getShell(), "Problem while opening the PDF File", //$NON-NLS-1$
 					e.getMessage());
 		}
 		return done;
@@ -157,8 +149,7 @@ public class ImportPDFHandler extends AbstractHandler implements IHandler {
 	 *         otherwise.
 	 */
 	private boolean editOperations() {
-		CheckAndEditOperationWizard wizard = new CheckAndEditOperationWizard(
-				addedOperations, account);
+		CheckAndEditOperationWizard wizard = new CheckAndEditOperationWizard(addedOperations, account);
 		WizardDialog wizardDialog = new WizardDialog(shell, wizard);
 		if (wizardDialog.open() == Window.CANCEL) {
 			// Abort all changes

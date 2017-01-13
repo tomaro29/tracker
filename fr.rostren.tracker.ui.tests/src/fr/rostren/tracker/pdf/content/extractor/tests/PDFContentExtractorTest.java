4845471c09cf071ad6081dca9444cf3342a66b5f
maro <maro2229@yahoo.fr>
2017-01-13 11:32:48 +0100

    Delete wrong copyright

--- fr.rostren.tracker.ui.tests/src/fr/rostren/tracker/pdf/content/extractor/tests/PDFContentExtractorTest.java
+++ fr.rostren.tracker.ui.tests/src/fr/rostren/tracker/pdf/content/extractor/tests/PDFContentExtractorTest.java
@@ -1,7 +1,3 @@
-/*******************************************************************************
- * Intel(R) CoFluent(TM) Studio - Intel Corporation
- * Copyright (C) 2003-2016, Intel Corporation. All rights reserved.
- *******************************************************************************/
 package fr.rostren.tracker.pdf.content.extractor.tests;
 
 import static org.junit.Assert.assertEquals;
